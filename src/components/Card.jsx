9bc2674ce0fb5c45e8b4eeb48a8b7183dec05b00
Jesus Diaz <jesusdiazh11@gmail.com>
2022-10-13 13:01:56 -0500

    Nuevo componente y estructura HTML

---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="container-fluid">
+      <div className="row row-cols-2 d-flex justify-content-around">
+        <div className="row d-flex justify-content-center">
+          <h4 className="">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste optio
+            doloremque exercitationem ducimus fuga voluptatibus dolores rerum,
+            debitis aperiam temporibus sunt repellendus ipsa ullam amet
+            explicabo quidem ab culpa voluptatum!
+          </h4>
+          <img />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/components/Peticion.jsx
+++ src/components/Peticion.jsx
@@ -1,12 +1,19 @@
 import { useEffect, useState } from "react";
+import Card from "./Card";
 
 const Peticion = () => {
-    
   return (
     <div className="text-center width-100">
-        <h1 className="">PETICION AL API DE BREAKING BAD</h1>
+      <h1 className="">PETICION AL API DE BREAKING BAD</h1>
+      <div className="container-sm d-flex justify-content-evenly">
+        <button className="btn btn-outline-danger">Anterior</button>
+        <button className="btn btn-outline-success">Siguiente</button>
+      </div>
+      <div className="container">
+      <Card/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Peticion
+export default Peticion;
