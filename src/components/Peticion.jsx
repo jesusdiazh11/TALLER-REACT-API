d83c6bbd0d64185d2e5574d8ce8fdbfc0e33bad3
Jesus Diaz <jesusdiazh11@gmail.com>
2022-10-13 12:47:32 -0500

    link de bootstrap y limpieza del codigo

--- src/components/Peticion.jsx
+++ src/components/Peticion.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
 const Peticion = () => {
+    
   return (
     <div className="text-center width-100">
         <h1 className="">PETICION AL API DE BREAKING BAD</h1>
