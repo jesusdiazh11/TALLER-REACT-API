ef8d033229044ef228588c7a214ea8bf996fced8
Jesus Diaz <jesusdiazh11@gmail.com>
2022-10-13 12:22:53 -0500

    Second Commit

---
+++ src/components/Peticion.jsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+
+const Peticion = () => {
+  return (
+    <div className="text-center width-100">
+        <h1 className="">PETICION AL API DE BREAKING BAD</h1>
+    </div>
+  )
+}
+
+export default Peticion
